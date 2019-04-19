ea791538519652e177fafefc389c5e420e71dea5
Sirko2097 <2097_Cthusq>
2019-04-19 18:19:23 +0300

    Added fourth task with streams.

---
+++ src/main/java/com/company/fourthtask/MainFourthTask.java
@@ -0,0 +1,29 @@
+package com.company.fourthtask;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MainFourthTask {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 0, -2, 23, -233, 0, 233};
+
+
+        IntStream stream = Arrays.stream(arr);
+        System.out.println("Average: " + stream.average().getAsDouble());
+
+        stream = Arrays.stream(arr);
+        System.out.println("Min: " + stream.min().getAsInt());
+
+        stream = Arrays.stream(arr);
+        System.out.println("Elem == 0: " + stream.filter((n) -> n == 0).count());
+
+        stream = Arrays.stream(arr);
+        System.out.println("Elem > 0: " + stream.filter((n) -> n > 0).count());
+
+        stream = Arrays.stream(arr);
+        arr = stream.map((n) -> (n * 3)).toArray();
+        System.out.println("arr * 3: " + Arrays.toString(arr));
+
+    }
+}
