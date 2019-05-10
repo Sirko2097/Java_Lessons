b8f28a8f572e366b62bb95a9d33bd8d00a538f39
Sirko2097 <2097_Cthusq>
2019-05-11 00:26:49 +0300

    Memento pattern example.

---
+++ src/main/java/com/company/designpatterns/memento/Game.java
@@ -0,0 +1,24 @@
+package com.company.designpatterns.memento;
+
+import lombok.ToString;
+
+@ToString
+public class Game {
+
+    private String level;
+    private int ms;
+
+    public void set(String level, int ms) {
+        this.level = level;
+        this.ms = ms;
+    }
+
+    public void load(Save save) {
+        level = save.getLevel();
+        ms = save.getMs();
+    }
+
+    public Save save() {
+        return new Save(level, ms);
+    }
+}
