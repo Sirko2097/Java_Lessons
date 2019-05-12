ac21a2839702900d8a1682d407b5770f89851bb0
Sirko2097 <2097_Cthusq>
2019-05-12 15:45:17 +0300

    Proxy pattern example.

---
+++ src/main/java/com/company/designpatterns/proxy/Image.java
@@ -0,0 +1,6 @@
+package com.company.designpatterns.proxy;
+
+public interface Image {
+
+    void display();
+}
---
+++ src/main/java/com/company/designpatterns/proxy/ProxyImage.java
@@ -0,0 +1,20 @@
+package com.company.designpatterns.proxy;
+
+public class ProxyImage implements Image {
+
+    private String file;
+
+    private RealImage image;
+
+    public ProxyImage(String file) {
+        this.file = file;
+    }
+
+    @Override
+    public void display() {
+        if (image == null) {
+            image = new RealImage(file);
+        }
+        image.display();
+    }
+}
